4347e38b613bffdd7700b1a57bfee19bf051b800
José Romualdo <joseromualdo@outlook.com>
2025-05-14 10:06:17 -0300

    Add project files.

---
+++ 01-Singlenton/ContaCorrente.cs
@@ -0,0 +1,49 @@
+﻿namespace PadroesProjeto.Singleton
+{
+    public class ContaCorrente
+    {
+        private static ContaCorrente _instance;
+
+        public string Numero { get; set; }
+        public string Agencia { get; set; }
+        public decimal Saldo { get; private set; }
+
+        private ContaCorrente(string numero, string agencia, decimal saldo)
+        {
+            Numero = numero;
+            Agencia = agencia;
+            Saldo = saldo;
+        }
+
+        public static ContaCorrente ObterInstancia()
+        {
+            if(_instance == null)
+            {
+                _instance = new ContaCorrente("1", "2025", 560.60m);
+            }
+            return _instance;
+        }
+
+        public void Depositar(decimal valor)
+        {
+            Saldo += valor;
+        }
+        public void Sacar(decimal valor)
+        {
+            if (valor > Saldo)
+            {
+                throw new InvalidOperationException("Saldo insuficiente.");
+            }
+            Saldo -= valor;
+        }
+
+        public void Debitar(decimal valor)
+        {
+            if (valor > Saldo)
+            {
+                throw new InvalidOperationException("Saldo insuficiente.");
+            }
+            Saldo -= valor;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Globalization;
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+using PadroesProjeto.Singleton;
+
+namespace PadroesProjeto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            #region Singleton
+            //neste momento vamos pagar a 
+            //conta do cafe da manha
+            var contaCM = ContaCorrente.ObterInstancia();
+
+            contaCM.Debitar(45.50m);
+
+            CultureInfo culture = new CultureInfo("pt-BR");
+            Thread.CurrentThread.CurrentCulture = culture;
+
+            Console.WriteLine($"Saldo: {contaCM.Saldo:C2}");
+
+            var contaAlmoco = ContaCorrente.ObterInstancia();
+            contaAlmoco.Debitar(52.25m);
+            Console.WriteLine($"Saldo: {contaAlmoco.Saldo:C2}");
+
+            //Exemplo de container de DI
+            //HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
+            //builder.Services.AddSingleton<ContaCorrente>(); 
+            #endregion
+
+
+        }
+    }
+}
